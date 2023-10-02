ff2299614ae0795ad14043f943931420f097cc1e
Arvinder <arvinder.dhanoa@winona.edu>
2023-10-02 14:54:35 -0500

    refactor(scheduler): make SystemState be passed in as a parameter.
    We do this because multiple schedulers might be running at the same
    time, and the virtue of simply the scheduler running doesnn't mean
    nothing else has in that time period. That is to say: calling tick() on
    a cpu scheduler shouldn't advance system time if the I/O scheduler could
    still do something, or one of the other cores.

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -10,7 +10,6 @@ struct FCFS {
     // is nice for this.
     processes: VecDeque<Process>,
     finished: Vec<Process>,
-    system_state: SystemState,
 }
 
 impl FCFS {
@@ -18,99 +17,109 @@ impl FCFS {
         processes.sort_by(|a,b| a.arrival.cmp(&b.arrival));
         Self {
             processes: processes.into(),
-            system_state: SystemState::new(),
             finished: Vec::new(),
         }
     }
 }
 
 impl Scheduler for FCFS {
-    fn tick(&mut self) {
+    fn tick(&mut self, system_state: &SystemState) {
        let process = match self.processes.get_mut(0) {
             Some(v) => v,
-            None => { self.system_state.time += 1; return;},
+            None => return,
         };
         // if the next process to work on isn't ready yet -
         // increment the system time and return, since we
         // don't have anything to do.
-        if process.arrival > self.system_state.time {
-            self.system_state.time += 1;
+        if process.arrival > system_state.time {
             return;
         }
-        process.tick(&self.system_state);
+        process.tick(system_state);
         if process.burst == 0 {
             // we've been working with the first process this entire time -
             // we know it exists so it's safe to just `unwrap()` it rather
             // then checking if it's there or not.
             self.finished.push(self.processes.pop_front().unwrap());
         }
-        self.system_state.time += 1;
     }
 }
 
 trait Scheduler {
-    fn tick(&mut self);
+    fn tick(&mut self, system_state: &SystemState);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    // this shouldn't panic, or do anything really.
     #[test]
     fn test_fcfs_zero_process() {
         let mut sched = super::FCFS::new(Vec::new());
-        sched.tick();
-        assert_eq!(sched.system_state.time, 1);
+        let state = SystemState::new();
+        sched.tick(&state);
     }
     #[test]
     fn test_fcfs_one_process() {
+        let mut state = SystemState::new();
         let mut sched = super::FCFS::new(vec![Process::new(String::from("test"), 0, 0, 10, 0)]);
         for _ in 0..10 {
-            sched.tick();
+            sched.tick(&state);
+            state.time += 1;
         }
         assert_eq!(sched.finished.len(), 1);
     }
     #[test]
     fn test_fcfs_one_process_different_arrival_time() {
+        let mut state = SystemState::new();
         let mut sched = super::FCFS::new(vec![Process::new(String::from("test"), 0, 0, 10, 2)]);
         for _ in 0..10 {
-            sched.tick();
+            sched.tick(&state);
+            state.time += 1;
         }
         assert_eq!(sched.processes[0].burst, 2);
         for _ in 0..2 {
-            sched.tick();
+            sched.tick(&state);
+            state.time += 1;
         }
         assert_eq!(sched.finished.len(), 1);
     }
     #[test]
     fn test_fcfs_multiple_processes() {
+        let mut state = SystemState::new();
         let mut sched = super::FCFS::new(vec![
             Process::new(String::from("test"), 0, 0, 10, 0),
             Process::new(String::from("test2"), 1, 1, 7, 5),
         ]);
         for _ in 0..10 {
-            sched.tick();
+            sched.tick(&state);
+            state.time += 1;
         }
         assert_eq!(sched.finished.len(), 1);
         for _ in 0..7 {
-            sched.tick();
+            sched.tick(&state);
+            state.time += 1;
         }
         assert_eq!(sched.finished.len(), 2);
     }
     #[test]
     fn test_fcfs_multiple_processes_with_idle() {
+        let mut state = SystemState::new();
         let mut sched = super::FCFS::new(vec![
             Process::new(String::from("test"), 0, 0, 10, 0),
             Process::new(String::from("test2"), 1, 1, 7, 11),
         ]);
         for _ in 0..10 {
-            sched.tick();
+            sched.tick(&state);
+            state.time += 1;
         }
         assert_eq!(sched.finished.len(), 1);
         for _ in 0..7 {
-            sched.tick();
+            sched.tick(&state);
+            state.time += 1;
         }
         assert_eq!(sched.finished.len(), 1);
-        sched.tick();
+        sched.tick(&state);
+        state.time += 1;
         assert_eq!(sched.finished.len(), 2);
     }
 }
